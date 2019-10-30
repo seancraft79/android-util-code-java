176e07ce0652fd29d771ed6db4a8584d39cfa590
Sean Lee <seancraft79@gmail.com>
2019-10-30 12:03:16 +0900

    Add SoftKeyboard StringUtil

---
+++ codeutillib/src/main/java/com/sean/codeutillib/StringUtil.java
@@ -0,0 +1,26 @@
+package com.sean.codeutillib;
+
+public class StringUtil {
+    public static boolean isNullOrEmpty(String str){
+
+        if(str == null || str.isEmpty() || str.length() < 1)
+            return true;
+
+        str = removeSpace(str);
+        if(str.isEmpty() || str.length() < 1)
+            return true;
+
+        return false;
+    }
+
+    public static String removeSpace(String s) {
+
+        String withoutspaces = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ')
+                withoutspaces += s.charAt(i);
+
+        }
+        return withoutspaces;
+    }
+}
