ac160149dad61542d15ae87655f2082596cef87e
Sean Lee <seancraft79@gmail.com>
2020-01-15 15:15:14 +0900

    Added ImageUtil

---
+++ lib_code_java/src/main/java/com/sean/lib_code_java/ImageUtil.java
@@ -0,0 +1,26 @@
+package com.sean.lib_code_java;
+
+import android.graphics.Bitmap;
+
+public class ImageUtil {
+
+    public static Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+}
