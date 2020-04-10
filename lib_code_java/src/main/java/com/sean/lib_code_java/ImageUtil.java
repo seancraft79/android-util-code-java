f225a427e4deeadfc9c5e8660f667d490e3644a6
Sean Lee <seancraft79@gmail.com>
2020-04-10 13:00:45 +0900

    Add saveImage to ImageUtil

--- lib_code_java/src/main/java/com/sean/lib_code_java/ImageUtil.java
+++ lib_code_java/src/main/java/com/sean/lib_code_java/ImageUtil.java
@@ -31,4 +31,28 @@ public class ImageUtil {
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
+
+    public static void saveBitmap(Context context, Bitmap finalBitmap, String filePath) {
+
+        File file = new File(filePath);
+        if (file.exists()) file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            // sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED,
+            // Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Tell the media scanner about the new file so that it is immediately available to the user.
+        MediaScannerConnection.scanFile(context, new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.d("ExternalStorage", "Scanned " + path + ": -> uri=" + uri);
+                    }
+                });
+    }
 }
